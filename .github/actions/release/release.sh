cf89d1d4accfbeadce63591b0479524a4965e040
Dave Schmitz <dgschmitz@symplr.com>
2024-01-02 09:13:55 -0500

    chore: add github cli command to shell script

--- .github/actions/release/release.sh
+++ .github/actions/release/release.sh
@@ -4,6 +4,14 @@ echo "Tag Value $1"
 echo "Repository $2"
 echo "INPUT_TAG_VALUE: $INPUT_TAG_VALUE"
 echo "INPUT_REPOSITORY: $INPUT_REPOSITORY"
+
+gh release view $1 --repo=$2
+if [[ $? -ne 0 ]]
+then
+    echo "Tag $1 was not found in repository $2"
+else
+    echo "Tag $1 was found in repository $2"
+fi
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 echo "HELLO_TIME=$time" >> $GITHUB_ENV
