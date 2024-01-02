7305284d2ec17dc077f7fd273ca4dfe78aa04193
Dave Schmitz <dgschmitz@symplr.com>
2024-01-02 10:12:41 -0500

    chore: add ability to create release

--- .github/actions/release/release.sh
+++ .github/actions/release/release.sh
@@ -11,4 +11,13 @@ else
     echo "Tag $1 was not found in repository $2"
 fi
 
+if gh release create $1 --repo=$2 --title=$1 --generate-notes
+then
+    echo "Release with tag $1 created in repository $2"
+    exit 0
+else
+    echo "Error occurred while trying to create release with tag $1 in repository $2"
+    exit 1
+fi
+
 echo "End of script"
