3bc599d6f2407a8a48d9ac79b732bcebbbdb93e6
Dave Schmitz <dgschmitz@symplr.com>
2024-01-02 09:52:06 -0500

    chore: update release script

--- .github/actions/release/release.sh
+++ .github/actions/release/release.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 echo "::debug::Running release.sh"
-echo "Tag Value $1"
-echo "Repository $2"
-echo "INPUT_TAG_VALUE: $INPUT_TAG_VALUE"
-echo "INPUT_REPOSITORY: $INPUT_REPOSITORY"
+echo "::debug::Tag Value $1"
+echo "::debug::Repository $2"
 
 if gh release view $1 --repo=$2
-# if [[ $? -ne 0 ]]
 then
-    echo "Tag $1 was found in repository $2"
+    echo "Release with tag $1 already exists in repository $2"
 else
     echo "Tag $1 was not found in repository $2"
 fi
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
-echo "HELLO_TIME=$time" >> $GITHUB_ENV
+
+echo "End of script
